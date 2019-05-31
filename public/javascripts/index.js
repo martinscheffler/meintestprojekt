82c6765d791965544617481514d10fb6b28d9d05
mscheff6 <martin.scheffler@t-systems.com>
2019-05-31 08:43:05 +0200

    fix

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,80 +1,79 @@
-$(document).ready(function () {
-  var timeData = [],
-    temperatureData = [],
-    humidityData = [];
-  var data = {
-    labels: timeData,
-    datasets: [
-      {
-        fill: false,
-        label: 'Temperature',
-        yAxisID: 'Temperature',
-        borderColor: "rgba(255, 204, 0, 1)",
-        pointBoarderColor: "rgba(255, 204, 0, 1)",
-        backgroundColor: "rgba(255, 204, 0, 0.4)",
-        pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
-        pointHoverBorderColor: "rgba(255, 204, 0, 1)",
-        data: temperatureData
-      },
-      {
-        fill: false,
-        label: 'Humidity',
-        yAxisID: 'Humidity',
-        borderColor: "rgba(24, 120, 240, 1)",
-        pointBoarderColor: "rgba(24, 120, 240, 1)",
-        backgroundColor: "rgba(24, 120, 240, 0.4)",
-        pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
-        pointHoverBorderColor: "rgba(24, 120, 240, 1)",
-        data: humidityData
-      }
-    ]
-  }
 
-  var basicOption = {
-    title: {
-      display: true,
-      text: 'Temperature & Humidity Real-time Data',
-      fontSize: 36
+var timeData = [],
+  temperatureData = [],
+  humidityData = [];
+var data = {
+  labels: timeData,
+  datasets: [
+    {
+      fill: false,
+      label: 'Temperature',
+      yAxisID: 'Temperature',
+      borderColor: "rgba(255, 204, 0, 1)",
+      pointBoarderColor: "rgba(255, 204, 0, 1)",
+      backgroundColor: "rgba(255, 204, 0, 0.4)",
+      pointHoverBackgroundColor: "rgba(255, 204, 0, 1)",
+      pointHoverBorderColor: "rgba(255, 204, 0, 1)",
+      data: temperatureData
     },
-    scales: {
-      yAxes: [{
-        id: 'Temperature',
+    {
+      fill: false,
+      label: 'Humidity',
+      yAxisID: 'Humidity',
+      borderColor: "rgba(24, 120, 240, 1)",
+      pointBoarderColor: "rgba(24, 120, 240, 1)",
+      backgroundColor: "rgba(24, 120, 240, 0.4)",
+      pointHoverBackgroundColor: "rgba(24, 120, 240, 1)",
+      pointHoverBorderColor: "rgba(24, 120, 240, 1)",
+      data: humidityData
+    }
+  ]
+}
+
+var basicOption = {
+  title: {
+    display: true,
+    text: 'Temperature & Humidity Real-time Data',
+    fontSize: 36
+  },
+  scales: {
+    yAxes: [{
+      id: 'Temperature',
+      type: 'linear',
+      scaleLabel: {
+        labelString: 'Temperature(C)',
+        display: true
+      },
+      position: 'left',
+    }, {
+        id: 'Humidity',
         type: 'linear',
         scaleLabel: {
-          labelString: 'Temperature(C)',
+          labelString: 'Humidity(%)',
           display: true
         },
-        position: 'left',
-      }, {
-          id: 'Humidity',
-          type: 'linear',
-          scaleLabel: {
-            labelString: 'Humidity(%)',
-            display: true
-          },
-          position: 'right'
-        }]
-    }
+        position: 'right'
+      }]
   }
+}
 
-  //Get the context of the canvas element we want to select
-  var ctx = document.getElementById("myChart").getContext("2d");
-  var optionsNoAnimation = { animation: false }
-  var myLineChart = new Chart(ctx, {
-    type: 'line',
-    data: data,
-    options: basicOption
-  });
-
-  /*var ws = new WebSocket('wss://' + location.host);
-  ws.onopen = function () {
-    console.log('Successfully connect WebSocket');
-  }*/
-  
+//Get the context of the canvas element we want to select
+var ctx = document.getElementById("myChart").getContext("2d");
+var optionsNoAnimation = { animation: false }
+var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: data,
+  options: basicOption
 });
 
+/*var ws = new WebSocket('wss://' + location.host);
+ws.onopen = function () {
+  console.log('Successfully connect WebSocket');
+}*/
 
-wsonmessage = function (data) {
+
+
+var wsonmessage = function (data) {
   console.log('receive message' + data);
   try {
     var obj = JSON.parse(data);
